b73a26d42d55734459bce517b238fba7b751052f
Gonzalo de Andrade <gonzadeandrade1@gmail.com>
2024-10-29 10:49:53 -0300

    componentes
    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavComponent } from "./components/nav/nav.component";
+import { BodyComponent } from "./components/body/body.component";
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavComponent, BodyComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
